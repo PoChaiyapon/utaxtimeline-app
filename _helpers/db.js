eee1c372342adac0b92a53e18e13ffa3f337d004
Chaiyapon <chaiporn137@gmail.com>
2021-09-07 14:25:51 +0700

    update commit

--- _helpers/db.js
+++ _helpers/db.js
@@ -4,39 +4,6 @@ const { Sequelize } = require('sequelize');
 
 // module.exports = db = {};
 
-// // initialize();
-
-// async function initialize() {
-//     // create db if it doesn't already exist
-//     const { host, port, user, password, database } = config.database;
-//     const connection = await mysql.createConnection({ host, port, user, password });
-//     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
-
-//     // connect to db
-//     const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
-
-//     // init models and add them to the exported db object
-//     db.User = require('../users/user.model')(sequelseize);
-//     //table event
-//     db.Event = require('../events/event.model')(sequelize);
-
-//     // sync all models with database
-//     await sequelize.sync();
-// }
-
-// const { host, port, user, password, database } = config.database;
-// const connection = await mysql.createConnection({ host, port, user, password });
-// await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
-
-// connect to db
-// const sequelize = new Sequelize(database, user, password, { 
-//   dialect: 'mysql',
-//   dialectOptions: {
-//         useUTC: false //for reading from database
-//     },
-//   timezone: '+07:00' //for writing to database
-// });
-
 const sequelize = new Sequelize({
     dialect: "mysql",
     host: "192.168.7.9",
--- events/event.service.js
+++ events/event.service.js
@@ -2,6 +2,7 @@ const config = require('config.json');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const db = require('_helpers/db');
+const { Op } = require("sequelize");
 
 module.exports = {
   getEventByOwner,
@@ -24,7 +25,49 @@ async function create(params){
     throw 'Owner id: "' + params.owner + '" not found';
   }
 
+  //check วันที่ start > end
+  if(CheckStartMoreEndDateTime(params)){
+    throw `[${params.startDate}] > [${params.endDate}] Start > End!`;
+  }
+  //check วันที่ซ้ำ
+  if(await CheckEventDateTime(params)){
+    throw `[${params.startDate}] Time is already!, ช่วงเวลานี้ระบุแล้ว!`;
+  }
+
   //save event
   await db.Event.create(params);
 
+}
+
+async function CheckEventDateTime(params) {
+    let countDup = await db.Event.count({ where: {
+    owner: params.owner,
+    [Op.or]:[{
+        startDate: {
+          [Op.between]: [params.startDate, params.endDate] // gte มากกว่าหรือเท่ากับ >=
+      }
+    },
+    {
+        endDate: {
+          [Op.between]: [params.startDate, params.endDate] // lte น้อยกว่าหรือเท่ากับ >=
+      }
+    }
+    ]
+  }});
+
+  if(countDup > 0 )
+    return true; //console.log('yes');
+  else 
+    return false; //console.log('noo');
+}
+
+function CheckStartMoreEndDateTime(params) {
+  const start = new Date(params.startDate);
+  const end = new Date(params.endDate);
+  if(end <= start){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
